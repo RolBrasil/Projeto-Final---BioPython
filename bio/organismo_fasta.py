1cdb49ee9686a5dcbbdae9bd278fcd0192cb3c2e
RolBrasil <rol.rodrigo1@gmail.com>
2024-07-19 19:56:35 -0300

    Update organismo_fasta.py

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,4 +1,5 @@
 class OrganismoFasta:
-    """
-    Criar
-    """
+    def __init__(self, id_, nome, sequencia):
+        self.id = id_
+        self.nome = nome
+        self.sequencia = Sequencia(sequencia)
